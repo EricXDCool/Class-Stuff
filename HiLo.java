d3464b7b67f7bd3315379d27c3beec22f700d657
EricXDCool <ericxdcool@gmail.com>
2017-06-15 12:01:17 -0600

    Add files via upload

--- HiLo.java
+++ HiLo.java
@@ -16,10 +16,6 @@ public class HiLo
 		
 		int points = 1000;
 		
-		Random rand = new Random();
-		
-		int number = rand.nextInt(13) + 1;
-		
 		System.out.println("\n\tHigh Low GAME ");
 		
 		System.out.println("\n\tRULES");
@@ -27,26 +23,106 @@ public class HiLo
 		System.out.println("\tNumbers 8 through 13 are high");
 		System.out.println("\tNumber 7 is neither high or low");
 		
-		System.out.println("\n\tYou have " + points + " points.");
-		
-		System.out.println("\n\tYou have " + game(points) + " points.");
+		game(points);
 		
 	}
 
 	public static int game(int points) {
 		
+		int risk;
+		
 		int guess;
+		
+		Random rand = new Random();
+		
+		int number = rand.nextInt(13) + 1;
+		
+		String again;
+		
+		String yes = "y";
+		String no = "n";
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("\n\tYou have " + points + " points.");
+		
+		
+		
 		System.out.print("\n\tEnter points to risk: ");
+		risk = input.nextInt();
+		
+		while (risk <= points) {
+		
+		System.out.print("\n\tPredict (1 = High, 0 = Low) :");
 		guess = input.nextInt();
 		
-		return (points);
+		System.out.println("\n\tThe Number is : " + number);
 		
+		if (guess == 1 && number >= 8) {
+			
+			System.out.println("\n\tYou Win!");	
+			
+			points = risk * 2 + points;
+			
+		} else if (guess == 1 && number <= 6) {
+			
+			System.out.println("\n\tYou Lose.");	
+			
+			points = points - risk;
+			
+		} else if (guess == 0 && number <= 6) {
+			
+			System.out.println("\n\tYou Win!");	
+			
+			points = risk * 2 + points;
+			
+		} else if (guess == 1 && number >= 8) {
+			
+			System.out.println("\n\tYou Lose.");	
+			
+			points = points - risk;
+		} else { 
+		
+			System.out.println("\n\tYou Lose."); 
+		
+			points = points - risk;
+		
+		}
+		
+		if (points <= 0) {
+		
+			System.out.println("\n\tYou have no more points!");	
+			
+		}
+		
+		
+		System.out.print("\n\tPlay Again? <y/n> : ");
+		again = input.next();
+		
+		if (again.equals(yes)) {
+		
+			game(points);
+			
+		} else {
+		
+			
+			endLoop(points);
+		}
+	
+		
+		} 
+		
+		System.out.println("\n\tNot valid amount");
+		
+		return(points);
 	}
 
-
+	public static void endLoop(int points){
+	
+	System.out.println("\n\tThanks for Playing!");
+	
+	
+	}
 
 
 
